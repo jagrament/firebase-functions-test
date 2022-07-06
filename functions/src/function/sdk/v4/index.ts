0dade4cc8833705e0d884af736bc86f2bf866200
Koji Yamazaki <kouji.yamazaki.cv@nttdocomo.com>
2022-07-06 23:02:03 +0900

    tmp commit

--- functions/src/function/sdk/v4/index.ts
+++ functions/src/function/sdk/v4/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 
-export const sayGrootNested = functions
+export const sayGroot = functions
     .https.onRequest((request, response) => {
       functions.logger.info("Hello Groot!", {structuredData: true});
       response.send("<h1>I am Groot</h1>");
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,12 @@
 import * as functions from "firebase-functions";
-import {sayGrootNested} from "./function/sdk/v4";
+import {sayGroot} from "./function/sdk/v4";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
 
 
 // export const sayGroot = functions.https.onRequest( (request, response) => {
@@ -14,9 +14,14 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 //   response.send("<h1>I am Groot</h1>");
 // });
 
-const myKey = "main";
+// export const sdkV4 = {[]: sayGroot};
+// if(process.env.DEPLOY_BRANCH){
 
-export const sdkV4 = {
-  [myKey]: sayGrootNested,
-};
+// } else {
+// }
+// const branch: string =
 
+
+const config = functions.config();
+export const sdkV4 = {[config.branch.name]: sayGroot};
+// export const sdkV4 = {[branch]: sayGroot};
