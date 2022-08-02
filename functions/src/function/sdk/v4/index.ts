219016020dcbaf04dd4eedc19f7c361a949c9616
山崎光司 <kojipole@kojipole-mac-mini.local>
2022-08-02 23:49:16 +0900

    Isolation level Testing

--- functions/src/function/sdk/v4/index.ts
+++ functions/src/function/sdk/v4/index.ts
@@ -1,8 +1,10 @@
 import * as functions from "firebase-functions";
 
+const config = functions.config();
+
 export const testFunc = functions
     .https.onRequest((request, response) => {
       functions.logger.info("Hello!", {structuredData: true});
-      response.send("<h1>Hello</h1>");
+      response.send(`<h1>Hello ${config?.branch.name}</h1>`);
     });
 
